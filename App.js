d930874fe7e0c5a923a869871db881349b171f08
patriciamsro <patriciamsro@gmail.com>
2022-12-06 22:29:11 -0300

    First commit

---
+++ App.js
@@ -0,0 +1,82 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View, Image} from 'react-native';
+import DatePicker from 'react-native-date-picker';
+// import DateTimePicker from '@react-native-community/datetimepicker';
+
+export default App = () => {
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const [nasa, setNasa] = useState(null);
+
+  const api = () => {
+    fetch(
+      'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=' + date.getFullYear() + '-' 
+      + date.getMonth() + '-' + date.getDate()
+    )
+    .then(response => response.json())
+    .then(json => setNasa(json.url))
+    .catch(err => console.log("erro ", err));
+  };
+
+  return (
+    <View style={styles.container}>
+        <StatusBar/>
+        {!nasa ? (
+          <>
+            <Text style={styles.texto}>{date.toLocaleDateString('fr')}</Text>
+            <View style={styles.botao}/>
+            <Button title="Alterar data" onPress={() => setOpen(true)} />
+            <View style={styles.botao}/>
+            <Button title="Buscar" onPress={() => api()} style={styles.btn}/>
+            <DatePicker
+              modal
+              open={open}
+              date={date}
+              onConfirm={dataSelecionada => {
+                setOpen(false);
+                setDate(dataSelecionada);
+              }}
+              onCancel={() => {
+                setOpen(false)
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <Button 
+              style={styles.botao}
+              title="Buscar outra foto"
+              onPress={() => setNasa(null)} 
+              />
+            <Image
+              style={styles.imagem}
+              source={{uri: nasa}}
+              resizeMode="contain"  
+            />
+          </>
+        )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imagem: {
+    padding: 200,
+    flex: 1,
+  },
+  botao: {
+    padding: 15,
+  },
+  texto : {
+    color: 'orange',
+    fontWeight: 'bold',
+    fontSize: 20
+  }
+});
